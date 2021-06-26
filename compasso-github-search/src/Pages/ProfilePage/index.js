7f86596c71cac51eb63d0ab7da68aa99690d1938
Davi Garcia <davigs@hotmail.com>
2021-06-26 03:00:20 -0300

    add repos and starred promises

--- compasso-github-search/src/Pages/ProfilePage/index.js
+++ compasso-github-search/src/Pages/ProfilePage/index.js
@@ -1,18 +1,42 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
 import SearchBar from '../../components/SearchBar'
 import useGlobalContext from "../../Context/GlobalContext";
-
-
-
-import React from 'react'
-import axios from 'axios'
 import { BASE_URL, client_id, client_secret } from "../../utils";
-import { goToProfilePage } from "../../Routes/coordinators";
-import { useHistory } from 'react-router';
-
-
 
 const ProfilePage = () => {
-  const { user } = useGlobalContext()
+  const { user, repos, setRepos, starred, setStarred } = useGlobalContext()
+
+  useEffect(() => {
+    getRepos()
+    getStarred()
+  }, [user])
+
+  const getRepos = () => {
+    axios.get(`${BASE_URL}/${user.login}/repos?client_id=${client_id}&client_secret=${client_secret}`)
+      .then(res => setRepos(res.data))
+    }
+
+  const getStarred = () => {
+    axios.get(`${BASE_URL}/${user.login}/starred?client_id=${client_id}&client_secret=${client_secret}`)
+    .then(res => setStarred(res.data))
+  }
+
+  const reposList = repos.map(item => {
+    return (
+      <div>
+        <h3> { item.name } </h3>
+      </div>
+    )
+  })
+
+  const starredList = starred.map(item => {
+    return (
+      <div>
+        <h3> { item.name } </h3>
+      </div>
+    )
+  })
   
   return (
     <div>
@@ -24,13 +48,19 @@ const ProfilePage = () => {
         <div>
           <h2> side bar </h2>
           <img src="" alt="profile pic" />
-          <h3>user name</h3>
+          <h3> { user.name } </h3>
           <p>seguidores . seguindo</p>
         </div>
         <div>
           <div>
-            <p> Repositórios </p>
-            <p> Starred </p>
+            <h2> Repositórios </h2>
+            <div>
+              {reposList}
+            </div>
+            <h2> Starred </h2>
+            <div>
+              {starredList}
+            </div>
           </div>
           <div>
             <p> api return content </p>
